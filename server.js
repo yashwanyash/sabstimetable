3bb79e0137330071dc14a8886c572781d885e444
yashwan <yashwan353@gmail.com>
2025-03-17 18:29:32 +0530

    updated database connection to railway

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ app.use(cors());
 app.use(express.json()); // Allows Express to handle JSON requests
 
 // MySQL Connection
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Yashwan@135790",
     database: "CollegeTimetable"
+});*/
+const db = mysql.createConnection({
+    host: "hopper.proxy.rlwy.net",   // Public MySQL Host from Railway
+    port: 26036,                      // Correct Port from Railway
+    user: "root",                     // Your MySQL user (root)
+    password: "NFgBQAasPKmFSqJEiPOkKvdnOMVsMMGo",   // Copy from Railway MYSQLPASSWORD
+    database: "railway"     // Copy from Railway MYSQLDATABASE
 });
-
 db.connect(err => {
     if (err) {
         console.error("❌ Database connection failed:", err);
